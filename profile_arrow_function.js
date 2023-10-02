2b0cfe01706d4bfae98ebe54f2916f38b7d1dee6
mzaki876 <141600774+mzaki876@users.noreply.github.com>
2023-10-02 13:30:12 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile =(namaDepan, namaBelakang, email) =>{
+  console.log("Nama : " + namaDepan + ""+ namaBelakang)
+  console.log("Email : " + email)
+}
+
+profile("Mohammad", "Zaki", "zd00729@gmail.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function profile(namaDepan, namaBelakang, email) {
+  console.log("Nama : " + namaDepan + ""+ namaBelakang)
+  console.log("Email : " + email)
+}
+
+profile("Mohammad", "Zaki", "zd00729@gmail.com")
